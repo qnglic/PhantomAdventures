fe34248fcb3c871c3dfaa7e3ca35e72c0a49cc9a
agap1mou <hannahafogelberg@gmail.com>
2025-09-17 23:54:51 +0200

    Wrote script for escape key
    Checks for key input and if isPaused is true or false

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -13,11 +13,22 @@ public class PauseMenu : MonoBehaviour
     }
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(!isPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                UnPause();
+            }
+        }
     }
 
     public void Respawn()
     {
+        UnPause();
         SceneManager.LoadScene(currentScene.buildIndex);
     }
 
