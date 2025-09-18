821e86ef4e4096b5579ca7e2902570e70aafed35
agap1mou <hannahafogelberg@gmail.com>
2025-09-18 17:44:46 +0200

    fixed script
    with serializedfield youd have to put in object killzone everytime, now it finds d object when starting so when in prefabs it can be directly added to scene

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class CheckPoints : MonoBehaviour
 {
-    [SerializeField] Killzone killzone;
+    private Killzone killzone;
+
+    // Instead of manually putting in killzone to every checkpoint
+    private void Start()
+    {
+        killzone = FindFirstObjectByType<Killzone>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
