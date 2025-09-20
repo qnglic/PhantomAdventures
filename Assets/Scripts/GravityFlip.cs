d8215a3f63d4ba1fb5474e03df4e0e599d3b355d
agap1mou <hannahafogelberg@gmail.com>
2025-09-20 13:28:43 +0200

    wrote script
    changes gravity with -1, also flips the sprite up or down

--- Assets/Scripts/GravityFlip.cs
+++ Assets/Scripts/GravityFlip.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class GravityFlip : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Rigidbody2D playerRb;
+    private SpriteRenderer player;
+
+    private void Start()
     {
-        
+         player = playerRb.GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerRb.gravityScale *= -1;
+            player.flipY = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
