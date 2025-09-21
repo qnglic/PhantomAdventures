1d0a9bf6acfa4a3069701688a694c1d1f7659e6a
agap1mou <hannahafogelberg@gmail.com>
2025-09-21 18:54:12 +0200

    added sound

--- Assets/Scripts/GravityFlip.cs
+++ Assets/Scripts/GravityFlip.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class GravityFlip : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D playerRb;
+    [SerializeField] private AudioClip sound;
     private SpriteRenderer player;
+    private AudioSource audioSource;
 
     private void Start()
     {
+        audioSource = GetComponent<AudioSource>();
          player = playerRb.GetComponent<SpriteRenderer>();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            audioSource.PlayOneShot(sound, 2.0f);
             playerRb.gravityScale *= -1;
             player.flipY = true;
         }
