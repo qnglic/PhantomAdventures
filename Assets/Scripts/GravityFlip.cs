92c12583fd0820a8f260ef8748ee9211aaa70517
agap1mou <hannahafogelberg@gmail.com>
2025-09-21 22:01:50 +0200

    update in script
    in playermovements added a function that changes gravity of player and flips the sprite, also moves it up/down to avoid bug, being called in gravity script
    Also added conditions for raycast so it detects when standing on ground flipped

--- Assets/Scripts/GravityFlip.cs
+++ Assets/Scripts/GravityFlip.cs
@@ -1,26 +1,37 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class GravityFlip : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D playerRb;
     [SerializeField] private AudioClip sound;
-    private SpriteRenderer player;
     private AudioSource audioSource;
+    private GameObject obj;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
-         player = playerRb.GetComponent<SpriteRenderer>();
+        obj = GetComponent<GameObject>();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
+        PlayerMovement player = other.GetComponent<PlayerMovement>();
+
         if (other.CompareTag("Player"))
         {
+            player.GravityFlip();
             audioSource.PlayOneShot(sound, 2.0f);
-            playerRb.gravityScale *= -1;
-            player.flipY = true;
         }
+
+        // to avoid bug when player jumps on top of object
+
+        obj.SetActive(false);
+        Invoke("Reactivate", 1f);
+    }
+
+    private void Reactivate()
+    {
+        obj.SetActive(true);
     }
 
 }
