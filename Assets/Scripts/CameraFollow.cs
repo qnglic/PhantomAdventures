2efd8c2012a457d6efad7ef59ccd901b1c3553db
lnskngrn <lnskngrn@gmail.com>
2025-09-28 07:42:38 +0200

    No print

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -23,7 +23,7 @@ public class CameraFollow : MonoBehaviour
             temp.y *= -1;
         }
 
-        print(temp.x);
+        //print(temp.x);
         Vector3 newPosition = Vector3.Lerp(transform.position, target.position + temp, smoothing * Time.deltaTime);
         transform.position = newPosition;
     }
